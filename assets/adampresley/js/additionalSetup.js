2fadd671f859cbb0445ec42a3c5bed22bb3cdf86
Adam Presley <adam@adampresley.com>
2014-11-11 16:52:05 -0600

    Adding block to conditionally load disqus

--- assets/adampresley/js/additionalSetup.js
+++ assets/adampresley/js/additionalSetup.js
@@ -27,11 +27,13 @@ $(document).ready(function() {
 	/*
 	 * Disqus
 	 */
-	var disqus_shortname = "adampresleyblog";
+	if ($("#disqus_thread").length) {
+		var disqus_shortname = "adampresleyblog";
 
-	(function() {
-		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	})();
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
+	}
 })
